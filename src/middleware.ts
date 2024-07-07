5b987e1191176a6304f3fed6924bdad3a9eefcc3
Lakshmikanta Patra <frontendfixer@gmail.com>
2024-07-08 01:51:38 +0530

    fix: no animated logo in login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,6 +32,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|logo.png|icons).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|logo.png|wallet-money.json|icons).*)',
   ],
 };
